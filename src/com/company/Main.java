e2725dfc0ef26b9eb2807317ea509e9364ad46eb
joneszhuang <joneszhuang@gmail.com>
2018-03-01 16:52:00 -0500

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main {
         Simulation sim = new Simulation();
         System.out.println(sim.stroke);
         System.out.println(sim.hospital);
+        System.out.println("Hello World");
     }
 }
