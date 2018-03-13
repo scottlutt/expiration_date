0293e6d7ba39efa454fc3b83b732c4fc15566a55
Alex Woodruff <alexwoodruff@Alexs-MacBook-Pro-4.local>
2018-03-13 17:40:25 -0400

    new format. death count instead of turn count

--- src/com/company/Disease.java
+++ src/com/company/Disease.java
@@ -3,21 +3,21 @@ package com.company;
 public class Disease {
 
     private String name;
-    private int ucTurn;
-    private int hTurn;
-    private int tTurn;
+    private int numTurns;
+    private Facilities correctLocation;
+    //private int tTurn;
 
-    public Disease(String name, int ucTurn, int hTurn, int tTurn)
+    public Disease(String name, int numTurns, Facilities correctLocation)
     {
         this.name = name;
-        this.ucTurn = ucTurn;
-        this.hTurn = hTurn;
-        this.tTurn = tTurn;
+        this.numTurns = numTurns;
+        this.correctLocation = correctLocation;
+        //this.tTurn = tTurn;
     }
 
     @Override
     public String toString()
     {
-        return "Disease name: " + this.name + "     Urgent Care Turns: " + this.ucTurn + "      Hospital Turns: " + this.hTurn + "      Trauma Turns: " + this.tTurn;
+        return "Disease name: " + this.name + "     Number of Turns: " + this.numTurns + "      Correct Location: " + this.correctLocation;
     }
 }
--- src/com/company/Simulation.java
+++ src/com/company/Simulation.java
@@ -1,6 +1,6 @@
 package com.company;
 
 public class Simulation extends Main{
-    Disease stroke = new Disease("stroke", 7,4,2);
     Facilities hospital = new Facilities("Mount Nittany Hospital", 200);
+    Disease stroke = new Disease("stroke", 7, hospital);
 }
