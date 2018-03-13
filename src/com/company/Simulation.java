d5c09ff23ea69fd717f2ea8608ecf611885b5896
Alex Woodruff <alexwoodruff@Alexs-MacBook-Pro-4.local>
2018-03-13 17:48:19 -0400

    more diseases and facilities

--- src/com/company/Simulation.java
+++ src/com/company/Simulation.java
@@ -2,5 +2,10 @@ package com.company;
 
 public class Simulation extends Main{
     Facilities hospital = new Facilities("Mount Nittany Hospital", 200);
+    Facilities urgentCare = new Facilities("Med Express", 50);
+    Facilities trauma = new Facilities("Trauma center", 100);
+
     Disease stroke = new Disease("stroke", 7, hospital);
+    Disease heartAttack = new Disease("Heart Attack", 5, trauma);
+    Disease flu = new Disease("Flu", 1, urgentCare);
 }
