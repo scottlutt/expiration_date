8f99f75ded18328df5c3d762bcf3c7820d20a083
Scott Luttmann <scottlutt8@gmail.com>
2018-03-15 17:26:26 -0400

    made array of names

--- src/com/company/Simulation.java
+++ src/com/company/Simulation.java
@@ -9,8 +9,15 @@ public class Simulation extends Main{
         ArrayList<Facilities> facList = new ArrayList<Facilities>();
         ArrayList<Disease> disList = new ArrayList<Disease>();
         ArrayList<Patient> patList = new ArrayList<Patient>();
-        //String names[99] = {};
-
+        String names [];
+        names = new String[] {"Michael","Matthew","Jason","Christopher","David","John","Joseph","James","Robert","Joshua","Daniel",
+            "Justin","Brian","Ryan","William","Andrew","Thomas","Timothy","Jonathan","Eric","Adam","Kevin","Jeffrey","Richard",
+            "Nicholas","Anthony","Mark","Jeremy","Stephen","Scott","Steven","Brandon","Charles","Patrick","Aaron","Benjamin",
+            "Paul","Gregory","Nathan","Shawn","Jesse","Sean","Chad","Ava","Emily","Emma","Madison","Olivia","Isabella","Abigail",
+            "Hannah","Sarah","Alexis","Sophia","Grace","Elizabeth","Samantha","Kayla","Julia","Ella","Alyssa","Lauren","Anna","Mia",
+            "Chloe","Natalie","Morgan","Gianna","Taylor","Brianna","Brooke","Hailey","Ashley","Riley","Lily","Savannah","Mackenzie",
+            "Sydney","Paige","Victoria","Nevaeh","Jessica","Leah","Rachel","Addison","Megan","Gabriella","Angelina","Kylie","Alexandra",
+            "Kaitlyn","Lillian","Kaylee","Gabrielle","Allison","Makayla","Katherine","Katelyn","Jenna"};
 
         Facilities hospital = new Facilities("Mount Nittany Hospital", 200, "Hospital");
         Facilities urgentCare = new Facilities("Med Express", 50, "Urgent Care");
