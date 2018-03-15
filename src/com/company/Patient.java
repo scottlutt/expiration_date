28d5f9cb2c9b0703365ad7d827ec5510513b9a00
Scott Luttmann <scottlutt8@gmail.com>
2018-03-15 17:12:55 -0400

    added patient class

---
+++ src/com/company/Patient.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Patient {
+    String names = "Michael,Matthew,Jason,Christopher,David,John,Joseph,James,Robert,Joshua,Daniel,Justin,Brian,Ryan,William,Andrew,Thomas,Timothy,Jonathan,Eric,Adam,Kevin,Jeffrey,Richard,Nicholas,Anthony,Mark,Jeremy,Stephen,Scott,Steven,Brandon,Charles,Patrick,Aaron,Benjamin,Paul,Gregory,Nathan,Shawn,Jesse,Sean,Chad,Ava,Emily,Emma,Madison,Olivia,Isabella,Abigail,Hannah,Sarah,Alexis,Sophia,Grace,Elizabeth,Samantha,Kayla,Julia,Ella,Alyssa,Lauren,Anna,Mia,Chloe,Natalie,Morgan,Gianna,Taylor,Brianna,Brooke,Hailey,Ashley,Riley,Lily,Savannah,Mackenzie,Sydney,Paige,Victoria,Nevaeh,Jessica,Leah,Rachel,Addison,Megan,Gabriella,Angelina,Kylie,Alexandra,Kaitlyn,Lillian,Kaylee,Gabrielle,Allison,Makayla,Katherine,Katelyn,Jenna,";
+    ArrayList<String> patients = new ArrayList<String>();
+
+
+
+}
