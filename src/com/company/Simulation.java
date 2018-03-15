6a2979622bd2259db7f6e4def9e7cd54664e7588
Alex Woodruff <alexwoodruff@Alexs-MacBook-Pro-4.local>
2018-03-15 17:12:22 -0400

    made array lists

--- src/com/company/Simulation.java
+++ src/com/company/Simulation.java
@@ -1,16 +1,30 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Simulation extends Main{
 
     public void play()
     {
+        ArrayList<Facilities> facList = new ArrayList<Facilities>();
+        ArrayList<Disease> disList = new ArrayList<Disease>();
+
+
         Facilities hospital = new Facilities("Mount Nittany Hospital", 200, "Hospital");
         Facilities urgentCare = new Facilities("Med Express", 50, "Urgent Care");
         Facilities trauma = new Facilities("Trauma center", 100, "Trauma");
 
+        facList.add(hospital);
+        facList.add(urgentCare);
+        facList.add(trauma);
+
         Disease stroke = new Disease("stroke", 7, hospital);
         Disease heartAttack = new Disease("Heart Attack", 5, trauma);
         Disease flu = new Disease("Flu", 1, urgentCare);
+
+        disList.add(stroke);
+        disList.add(heartAttack);
+        disList.add(flu);
 
         //System.out.println(hospital);
         System.out.println(stroke+ " "+stroke.getCorrectLocation(hospital));
